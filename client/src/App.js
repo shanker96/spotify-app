9739c7347410c89cd549853a3f2859bc121f8b7a
Shanker Nagarajan <shan@Shankers-MacBook-Pro.local>
2023-03-30 14:24:02 -0500

    Added Login page

--- client/src/App.js
+++ client/src/App.js
@@ -7,18 +7,9 @@ import {
 } from 'react-router-dom';
 import { accessToken, logout,  getCurrentUserProfile } from './spotify';
 import { catchErrors } from './utils';
-import styled from 'styled-components/macro';
 import { GlobalStyle } from './styles';
-import './App.css';
+import {Login} from './pages'
 
-const StyledLoginButton = styled.a`
-background-color: var(--green);
-color: var(--white);
-padding: 10px 20px;
-margin: 20px;
-border-radius: 30px;
-display: inline-block;
-`;
 
 // Scroll to top of page when changing routes
 // https://reactrouter.com/web/guides/scroll-restoration/scroll-to-top
@@ -53,10 +44,7 @@ function App() {
 
       <header className="App-header">
       {!token ? (
-          <StyledLoginButton className="App-link"
-              href="http://localhost:8888/login">
-            Log in to Spotify
-          </StyledLoginButton>
+          <Login />
         ) : (
           <Router>
             <ScrollToTop />
