2deb40963d38da0c5d21d9b8ed38143e3af9250b
Shanker Nagarajan <shan@Shankers-MBP.lan>
2023-03-30 12:28:37 -0500

    Added sytled components in React

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,36 @@
 import { useEffect, useState } from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  useLocation,
+} from 'react-router-dom';
 import { accessToken, logout,  getCurrentUserProfile } from './spotify';
 import { catchErrors } from './utils';
+import styled from 'styled-components/macro';
+import { GlobalStyle } from './styles';
 import './App.css';
+
+const StyledLoginButton = styled.a`
+background-color: var(--green);
+color: var(--white);
+padding: 10px 20px;
+margin: 20px;
+border-radius: 30px;
+display: inline-block;
+`;
+
+// Scroll to top of page when changing routes
+// https://reactrouter.com/web/guides/scroll-restoration/scroll-to-top
+function ScrollToTop() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+}
 
 function App() {
   const [token, setToken] = useState(null);
@@ -20,30 +49,47 @@ function App() {
 
   return (
     <div className="App">
+      <GlobalStyle />
+
       <header className="App-header">
-        {!token ? (
-        <a
-          className="App-link"
-          href="http://localhost:8888/login"
-        >
-          Log in to Spotify
-        </a>
-        ): (
-          <>
-            <h1>Logged in!</h1>
-            <button onClick={logout}>Log Out</button>
+      {!token ? (
+          <StyledLoginButton className="App-link"
+              href="http://localhost:8888/login">
+            Log in to Spotify
+          </StyledLoginButton>
+        ) : (
+          <Router>
+            <ScrollToTop />
+            <Switch>
+              <Route path="/top-artists">
+                <h1>Top Artists</h1>
+              </Route>
+              <Route path="/top-tracks">
+                <h1>Top Tracks</h1>
+              </Route>
+              <Route path="/playlists/:id">
+                <h1>Playlist</h1>
+              </Route>
+              <Route path="/playlists">
+                <h1>Playlists</h1>
+              </Route>
+              <Route path="/">
+                <>
+                  <button onClick={logout}>Log Out</button>
 
-            {profile && (
-              <div>
-                <h1>{profile.display_name}</h1>
-                <p>{profile.followers.total} Followers</p>
-                {profile.images.length && profile.images[0].url && (
-                  <img src={profile.images[0].url} alt="Avatar" />
-                )}
-              </div>
-            )}
-
-          </>
+                  {profile && (
+                    <div>
+                      <h1>{profile.display_name}</h1>
+                      <p>{profile.followers.total} Followers</p>
+                      {profile.images.length && profile.images[0].url && (
+                        <img src={profile.images[0].url} alt="Avatar"/>
+                      )}
+                    </div>
+                  )}
+                </>
+              </Route>
+            </Switch>
+          </Router>
         )}
       </header>
     </div>
